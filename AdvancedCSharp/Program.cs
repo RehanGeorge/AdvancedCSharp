ad626da80b823930afaf303f224a30c8daec13f9
RehanGeorge <rehan.george@gmail.com>
2023-12-11 18:06:03 +0530

    Added a struct

--- AdvancedCSharp/Program.cs
+++ AdvancedCSharp/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace AdvancedCSharp
 {
+    // Structs do not support inheritance, but they can implement interfaces.
+    struct Game
+    {
+        public string name;
+        public string developer;
+        public double rating;
+        public string releaseDate;
+
+        public void Display()
+        {
+            Console.WriteLine($"Game 1's name is {name}");
+            Console.WriteLine($"Game 1's developer is {developer}");
+            Console.WriteLine($"Game 1's rating is {rating}");
+            Console.WriteLine($"Game 1's release date is {releaseDate}");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Game game1;
+
+            game1.name = "Pokemon Go";
+            game1.developer = "Niantic";
+            game1.rating = 4.5;
+            game1.releaseDate = "July 6, 2016";
+
+            game1.Display();            
         }
     }
 }
